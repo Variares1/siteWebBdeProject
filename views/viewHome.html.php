15d92c39fa531566f94190947c8eba69b3611c85
Acquart <quentin.acquart@viacesi.fr>
2019-01-21 17:34:28 +0100

    Home part 3 clean

--- views/viewHome.html.php
+++ views/viewHome.html.php
@@ -15,5 +15,10 @@ foreach ($donnees as $donnee) {
         <a href="./views/viewShop.html.php"><img src="https://leekwars.com/image/about/illustration.png" alt=""></a>
         <div class="events-home-text"><h2>Events</h2><strong style="color: green">Leek Wars</strong> est un jeu de combat de poireaux, par navigateur, dans lequel vous programmez votre intelligence artificielle pour réussir à vaincre vos ennemis !</div>
     </div>
+    <div class="ideas-home">
+        <div class="activities-home-text"><h2><i class="far fa-calendar-check"></i> Activities</h2><strong style="color: rgba(255, 165, 0, 0.8);">...</strong></div>
+        <div class="idea-home-text"><h2><i class="fas fa-box-open"></i> Idea Box <i class="far fa-lightbulb"></i></h2><strong style="color: #6586b9cc">...</strong></div>
+    </div>
 </div>
+
 <?php require_once('footer.inc.php') ?>
