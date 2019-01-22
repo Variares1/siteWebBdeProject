d65efe8c125afa484830725db269ea37dad98f07
Acquart <quentin.acquart@viacesi.fr>
2019-01-22 16:40:37 +0100

    login 1.0

--- views/viewSign.html.php
+++ views/viewSign.html.php
@@ -2,7 +2,9 @@
 
     <div class="container-page">
         <div class="sign">
-
+            <div class="sign-panel">
+                <h3>Login</h3>
+            </div>
         </div>
     </div>
 
