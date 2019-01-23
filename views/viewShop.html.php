7ab4413ec82a0c36f6384fcde4c945051b3f5e06
Acquart <quentin.acquart@viacesi.fr>
2019-01-23 17:50:33 +0100

    shop 2.0

--- views/viewShop.html.php
+++ views/viewShop.html.php
@@ -5,7 +5,7 @@
             <h1 id="default">
                 SHOP <br>
                 <form class="form-shop" action="">
-                    <input class="search-shop" type="search" placeholder="Our goodies" onclick="setupShop()">
+                    <input class="search-shop" type="search" placeholder="Search" oninput="setupShop()" onclick="setupShop()">
                     <input class="submit-shop" id="hide" type="submit" value="GO">
                 </form>
                 <i class="fas fa-search" id="hide"></i>
@@ -16,10 +16,12 @@
 <?php require_once('footer.inc.php') ?>
 
 <script>
+
     function setupShop(){
         let myPanel = document.querySelector("h1");
         let mySubmit = document.querySelector(".submit-shop");
         let mySearch = document.querySelector(".fa-search");
+        let mySb = document.querySelector(".search-shop");
         if(myPanel.id=="default"){
             myPanel.setAttribute("id", "clicked-panel");
             setTimeout(function(){
@@ -28,12 +30,16 @@
                     mySubmit.setAttribute("id", "clicked-submit");
                 }, 1000);
             }, 3000);
-            mySearch.setAttribute("id", "clicked-search");
 
         }
         // else if(myPanel.id=="hide"){
         //     myPanel.setAttribute("id", "show");
         // }
+        if (mySb.value == '')
+            setTimeout(function(){
+                mySearch.setAttribute("id", "clicked-search");
+            }, 3000);
+        else mySearch.setAttribute("id", "hide");
         return 0;
     }
 </script>
