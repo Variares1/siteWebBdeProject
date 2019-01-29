69573f80b4a0d157b539922b691fd7b7746c19bb
Acquart <quentin.acquart@viacesi.fr>
2019-01-29 20:51:51 +0100

    sign

--- views/signIn.php
+++ views/signIn.php
@@ -23,29 +23,29 @@ if(isset($role)){
 
 }
 
-$data2 = '{"id":"'.$email.'"}';
-
-$try2 = $curl->getT('users/:id',$data2, $token);
-
-echo '<br>';
-var_dump($try2);
-echo '<br>';
-
-$array2 = json_decode($try2, true);
-
-$name = $array2['name'];
-$firstName = $array2['firstName'];
-
-$_SESSION["name"]=$name;
-$_SESSION["firstName"]=$firstName;
-
-var_dump($_SESSION["session"]);
-echo '<br>';
-var_dump($_SESSION["token"]);
-echo '<br>';
-var_dump($_SESSION["name"]);
-echo '<br>';
-var_dump($_SESSION["firstName"]);
+//$data2 = '{"id":"'.$email.'"}';
+//
+//$try2 = $curl->getT('users/:id',$data2, $token);
+//
+//echo '<br>';
+//var_dump($try2);
+//echo '<br>';
+//
+//$array2 = json_decode($try2, true);
+//
+//$name = $array2['name'];
+//$firstName = $array2['firstName'];
+//
+//$_SESSION["name"]=$name;
+//$_SESSION["firstName"]=$firstName;
+//
+//var_dump($_SESSION["session"]);
+//echo '<br>';
+//var_dump($_SESSION["token"]);
+//echo '<br>';
+//var_dump($_SESSION["name"]);
+//echo '<br>';
+//var_dump($_SESSION["firstName"]);
 
 header('Location: http://localhost/siteWebBdeProject/');
 
